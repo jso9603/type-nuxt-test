b096c42699a9194dc01e3fe753f2eaeebed357b1
JISOO PARK <jso9603@gmail.com>
2019-11-07 17:29:40 +0900

    defined vuefity theme colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,11 +44,15 @@ export default {
   ],
   vuetify: {
      theme: {
-      primary: '#2c3e50',
-      secondary: '#1abc9c',
-      accent: '#2980b9',
-      error: '#e74c3c',
-      action: '#23DB2A'
+       themes: {
+         light: {
+            primary: '#2c3e50',
+            secondary: '#1abc9c',
+            accent: '#2980b9',
+            error: '#e74c3c',
+            action: '#23DB2A'
+         }
+       }
     }, 
     // customVariables: ['~/assets/style/app.scss']
   }
